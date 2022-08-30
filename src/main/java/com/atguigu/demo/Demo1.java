a46cbc58cb3b39aa91421db321c9548189d86afa
lsss <lsfirst@163.com>
2022-08-30 21:04:14 +0800

    第二次更新

--- src/main/java/com/atguigu/demo/Demo1.java
+++ src/main/java/com/atguigu/demo/Demo1.java
@@ -4,5 +4,6 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("新建测试");
         System.out.println("第一次更新");
+        System.out.println("1231231456123456");
     }
 }
